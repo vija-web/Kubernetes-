dcf5016baf1f8a7168a7c5874d805c647c8c7a9e
vijay <vijayanthony28667@gmail.com>
2026-05-19 11:59:03 +0530

    addded

---
+++ Basics/Installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# aws eksctl
+curl --silent --location \
+"https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" \
+| tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+## kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.3/2026-04-08/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
