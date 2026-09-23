684938d8a79a228743b0570ad1a4544bfe440a2d
vijay <vijayanthony28667@gmail.com>
2026-09-23 21:35:03 +0530

    Added

--- Basics/Installation.sh
+++ Basics/Installation.sh
@@ -11,3 +11,7 @@ sudo mv /tmp/eksctl /usr/local/bin
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.3/2026-04-08/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
